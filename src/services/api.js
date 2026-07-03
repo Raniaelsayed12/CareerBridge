e9e348fc3f16c44f6447d639d619f3fa57296b0c
Rania Elsayed <raniaqwertyuio@gmail.com  GitHub>
2026-07-03 20:14:26 +0300

    Connect frontend to Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
+  baseURL: "https://careerbridge-backend-kasx.onrender.com",
+});
+
+export default axios.create({
   baseURL: "http://localhost:3000",
 });
